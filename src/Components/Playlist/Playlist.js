78242b78037a99ed7e61648e74bcb0d89bb7d304
Blessing <bigbadumhe@gmail.com>
2022-08-31 22:49:51 -0500

    work so far

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ParentClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { totalClicks: 0 };
+  }
+
+  handleClick() {
+    const total = this.state.totalClicks;
+
+    // calling handleClick will 
+    // result in a state change:
+    this.setState(
+      { totalClicks: total + 1 }
+    );
+  }
+}
